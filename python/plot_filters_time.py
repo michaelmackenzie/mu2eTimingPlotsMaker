f82802fce11dffd7a54231d37732ddb466c76bae
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2025-12-16 09:00:21 -0600

    Add some comments and minor edits

--- python/plot_filters_time.py
+++ python/plot_filters_time.py
@@ -1,8 +1,6 @@
 import time
 import math
-#import numpy as np
 import matplotlib.pyplot as plt
-import glob
 
 from ROOT import TFile
 from importlib import import_module
@@ -10,86 +8,11 @@ from importlib import import_module
 import os, sys
 import argparse
 
-from concurrent.futures import ThreadPoolExecutor
-
-names  = { 
-    'producers' : { "makeSD",
-                    "CaloClusterFast",
-                    #"CaloDigiMaker",
-                    "CaloHitMakerFast",
-                   # "FastCaloHitMaker",
-                    "OfflineFragmentReader",
-                    "TTCalHelixFinderDe",
-                    "TTCalHelixMergerDe",
-                    "TTCalSeedFitDe",
-                   # "TTCalSeedFitDep",
-                    "TTCalTimePeakFinder",
-                    "TTflagPH",
-                    "TThelixFinder",
-                    "TTHelixMergerDe",
-                    "TTKSFDe",
-                    "TTTZClusterFinder",
-                    "TTAprHelixFinder",
-                    "TTAprHelixMerger",
-                    "TTAprKSF",
-                    "TTmakePH",
-                    "TTmakeSH",
-                    "TTmakeSTH",
-                    "TTtimeClusterFinder"
-                },
-    'prescalers' : {"cprLowPSeedDeEventPrescale",
-                    "cprSeedDeEventPrescale",
-                    "tprHelixCalibIPADeEventPrescale",
-                    "tprHelixIPADeEventPrescale",
-                    "tprHelixCalibIPADePrescale",
-                    "tprHelixIPADePrescale",
-                    "tprLowPSeedDeEventPrescale",
-                    "tprLowPSeedDePrescale",
-                    "tprLowPSeedDePEventPrescale",
-                    "tprSeedDeEventPrescale",
-                    "tprSeedDePrescale",
-                    "tprSeedDePEventPrescale",
-                },
-    
-                    
-    'filters_cpr0'   : {"cprLowPSeedDeHSFilter",
-                        "cprLowPSeedDeSDCountFilter",
-                        "cprLowPSeedDeTCFilter",
-                        "cprLowPSeedDeTSFilter"},
-    'filters_cor1'   :{ "cprSeedDeHSFilter",
-                        "cprSeedDeSDCountFilter",
-                        "cprSeedDeTCFilter",
-                        "cprSeedDeTSFilter"},
-
-    'filters_tpr0'   : {"tprHelixDeIpaPhiScaledHSFilter",
-                        "tprHelixCalibIPADeSDCountFilter",
-                        "tprHelixDeIpaPhiScaledTCFilter",
-                        "tprHelixDeIpaHSFilter",
-                        "tprHelixIPADeSDCountFilter",
-                        "tprHelixDeIpaTCFilter"},
-
-    'filters_tpr1'   : {"tprLowPSeedDeHSFilter",
-                        "tprLowPSeedDeSDCountFilter",
-                        "tprLowPSeedDeTCFilter",
-                        "tprLowPSeedDeTSFilter"},
-
-    'filters_tpr2'   : {"tprSeedDeHSFilter",
-                        "tprSeedDeSDCountFilter",
-                        "tprSeedDeTCFilter",
-                        "tprSeedDeTSFilter"},
-
-    'summary'    : {"tot_event_timing",
-                    "tot_event_timing_noFilters",
-                    "tot_timing_filters",
-                    "tot_timing_prescale",
-                    "tot_timing_SDFilter",
-                    "tot_timing_TCFilter",
-                    "tot_timing_HSFilter",
-                    "tot_timing_TSFilter"}
-}
+# The list of modules run keyed by module type
+names  = dict()
 
 
-
+#--------------------------------------------------------------------------------
 def parseArgs(argv):
     parser = argparse.ArgumentParser(prog='./plot_filter_time.py')
     
@@ -99,6 +22,7 @@ def parseArgs(argv):
         
     return args
 
+#--------------------------------------------------------------------------------
 def extract_info(filename, tag):
     file = TFile.Open(filename)
     
@@ -149,70 +73,75 @@ def organize_modules(modules):
             mod_cat['producers'].append(module)
     return mod_cat
 
+#-------------------------------------------------------------------------------
+# Make a bar plot of the timing by module
+def plot_module_timing(filename, out_dir, tag, use_visits = False):
 
-def plot_module_timing(args, tag, use_visits = False):
-    
-    labels, timing, timing_err, entries = extract_info(args.filename, tag)
+    # Get the data from the input file for each module
+    labels, timing, timing_err, entries = extract_info(filename, tag)
     max_entries      = max(entries) if len(entries) > 0 else 0
+    y_pos = range(len(labels))    
+
+    # If doing per event, scale the module times by N(visits) / N(events)
     if not use_visits and max_entries > 0:
         timing     = [timing    [index] * entries[index] / max_entries for index in range(len(entries))]
         timing_err = [timing_err[index] * entries[index] / max_entries for index in range(len(entries))]
+
+    # Configure the plot style
     plt.clf()
     plt.rcdefaults()
     plt.subplots_adjust(left=0.3,right=0.95, top=0.95, bottom=0.1)
 
+    # Configure the axes
     ax = plt.gca()
-    # ax.set_ymargin(left=0.3,right=0)
-    #y_pos = np.arange(len(labels))
-    y_pos = [] #np.arange(len(labels))
-
-    for i in range(len(labels)):
-        y_pos.append(i)
-    
     ax.barh(y_pos, timing, align='center', color='orange' if not use_visits else 'blue')
     ax.set_yticks(y_pos)
     ax.set_yticklabels(labels,fontsize=7)
     ax.invert_yaxis()  # labels read top-to-bottom
     if 'summary' not in tag:
-        if use_visits:
-            ax.set_xlabel('timing/module_visit [ms]')
-        else:
-            ax.set_xlabel('timing/module_event [ms]')
-    else:
-        ax.set_xlabel('timing/event [ms]')
+        if use_visits: ax.set_xlabel('timing/module_visit [ms]')
+        else:          ax.set_xlabel('timing/module_event [ms]')
+    else:              ax.set_xlabel('timing/event [ms]')
     ax.set_title('Modules {} timing chart'.format(tag))
 
+    # Add the text timing values to the plot
     for i, v in enumerate(timing):
         ax.text(v + 0., i - 0., str("%2.2f"%v), color='orange' if not use_visits else 'blue', fontweight='bold')
-    
-    # top = len(y_pos)*1.1
+
+    # Configure the axis ranges
     max_val = max(timing)
     plt.ylim(-0.5, len(timing)*1.08)
     plt.xlim(0., 1.1*max_val)
 
-    plt.text(0.05,0.97,r'$\mathbf{{Mu2e}}$ Preliminary',ha="left",va="top",family='sans-serif',transform=ax.transAxes,fontsize=13)
-    # plt.text(0.05,0.92,'ots',ha="left",va="top",family='sans-serif',transform=ax.transAxes,style='italic',fontsize=8)
-    # plt.text(0.05,0.88,r'$\sqrt{s}=13\,\mathrm{TeV}$',ha="left",va="top",family='sans-serif',transform=ax.transAxes)
 
-    # if flow_etau_VBF[0]>0:
-    #     plt.xscale('log')
-
+    # Print the plot in PDF and PNG
+    plt.text(0.05,0.97,r'$\mathbf{{Mu2e}}$ $\it{Preliminary}$',ha="left",va="top",family='sans-serif',transform=ax.transAxes,fontsize=13)
     print(f'Printing timing plot for tag {tag} with use visits = {use_visits}')
-    plt.savefig("{}/plot_timing_modules_{}.pdf".format(args.out_dir,tag+'_visit' if use_visits else tag))
-    plt.savefig("{}/plot_timing_modules_{}.png".format(args.out_dir,tag+'_visit' if use_visits else tag))
+    plt.savefig("{}/plot_timing_modules_{}.pdf".format(out_dir,tag+'_visit' if use_visits else tag))
+    plt.savefig("{}/plot_timing_modules_{}.png".format(out_dir,tag+'_visit' if use_visits else tag))
     
     return
 
-def plot_filters_time(args):
+#--------------------------------------------------------------------------------
+def plot_filters_time(filename, out_dir):
 
+    # For each module list category, plot the timing per visit and per event
     for key in sorted(names):
-        plot_module_timing(args, key, False)
-        plot_module_timing(args, key, True )
+        plot_module_timing(filename, out_dir, key, False)
+        plot_module_timing(filename, out_dir, key, True )
     
 #--------------------------------------------------------------------------------
 if __name__=="__main__":
+    # Parse the arguments and retrieve the parameters
     args = parseArgs(sys.argv[1:])
-    modules = modules_list('/'.join(args.filename.split('/')[:-1]))
+    filename = args.filename
+    out_dir = args.out_dir
+
+    # Get the list of modules and organize them by type
+    file_dir = '/'.join(filename.split('/')[:-1])
+    modules = modules_list(file_dir)
     names = organize_modules(modules)
-    plot_filters_time(args)
+
+    # Make the plots
+    plot_filters_time(filename, out_dir)
 
